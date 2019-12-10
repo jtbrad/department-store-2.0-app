96f6e08fa30b7946ae19001ffb343370557753c0
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-12-10 14:19:34 -0700

    added department cards to home page

---
+++ client/src/components/Department.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, } from 'semantic-ui-react';
+
+const Department = () => (
+  <Card>
+    <Card.Content>
+      <Card.Header>Department</Card.Header>
+    </Card.Content>
+  </Card>
+);
+
+export default Department;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,16 @@
 import React, { Fragment, } from 'react';
-import { Header, } from 'semantic-ui-react';
+import Department from './Department';
+import { Header, Card, } from 'semantic-ui-react';
 
 const Home = () => (
-  <Header as="h1">Home</Header>
+  <Fragment>
+    <Header as="h1">Rails React Department Store</Header>
+    <hr />
+    <Header as="h2">Departments</Header>
+    <Card.Group>
+      <Department />
+    </Card.Group>
+  </Fragment>
 );
 
 export default Home;
